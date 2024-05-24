d788305751f20d9dd3402bc33400fdb20309aa21
xi455 <a3083493@gmail.com>
2024-05-24 23:45:40 +0800

    feat: settings app url and add env variable setting

--- core/urls.py
+++ core/urls.py
@@ -22,8 +22,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('catalog/', include('library.urls')),
+    path('catalog/', include('catalog.urls', namespace="catalog")),
+    path('library/', include('library.urls', namespace="library")),
 ]
 
 urlpatterns += static(
