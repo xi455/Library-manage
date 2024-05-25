cf2fc6ef67959fe8eb885dd0eb8d54c4a5855d98
xi455 <a3083493@gmail.com>
2024-05-25 13:30:38 +0800

    feat: feat list and detail function for catalog

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.views import generic
+
+from catalog.models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'   # your own name for the list as a template variable
+    queryset = Book.objects.all() # Get 5 books containing the title war
+    template_name = 'catalog/book_list.html'  # Specify your own template name/location
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'catalog/book_detail.html'  # Specify your own template name/location
